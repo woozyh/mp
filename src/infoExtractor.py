c8e2993896579085bcee5a5f364a8a2058c39d30
woozy <woozyhh@gmail.com>
2025-08-26 20:40:12 +0330

    updating .gitignore

--- src/infoExtractor.py
+++ src/infoExtractor.py
@@ -7,33 +7,49 @@ from os import listdir
 class Info(object):
 
     # the frames that stored are those which helpfull not all.
-    FRAMES: dict[bytes, str] = {
-        "TIT2": "",
-        "TPE1": "",
-        "TALB": "",
-        "TYER": "",
-        "APIC": "",
-        "TCON": "",
+    FRAMES: dict[str, str | bytes] = {
+        "TIT2": "N/A",
+        "TPE1": "N/A",
+        "TALB": "N/A",
+        "TYER": "N/A",
+        "APIC": "N/A",
+        "TCON": "N/A",
     }
     MAGIC_NUMBERS: list[str] = ["fffb", "494433"]
     
-    def __init__(self, file_path: str):
+    def __init__(self,):
         """Initializing the Info calss."""
 
         self.isValid: bool = None
         self.fileType: str = None
         self.fileCursor: BufferedReader = None
         
-        try:
+    def process(self, file_path: str) -> None:
+        """processing the file."""
 
+        songFrames = self.FRAMES.copy()
+        
+        try:
             self.fileCursor = open(file_path, "rb")
-            self.fileType   = self.fileCursor.read(3).hex()
-            self.isValid    = self.validateFile()
-            self.fileSize   = path.getsize(file_path)
+            self.fileType = self.fileCursor.read(3).hex()
+            self.isValid  = self.validateFile()
+            self.fileSize = path.getsize(file_path)
+
+            if self.isValid:
+                self.readHeader()
+                if self.id3Version == b'\x03\x00' or b'\x04\x00':
+                    songFrames = self.findFrames()
+                    print("songFrames: ", songFrames)
+                    songFrames = self.exportImage(song_frames = songFrames)
+                    self.closeFile()
+            else:
+                print(f"This {file_path} song doesn't support id3Tag.")
             
         except (FileNotFoundError, IsADirectoryError) as er:
-            pass            
-
+            pass
+        
+        return songFrames
+        
     def validateFile(self,):
         """Validating file with magicNumber."""
 
@@ -49,7 +65,7 @@ class Info(object):
     def findFrames(self,) -> dict:
         """Matching each fram from specified file."""
 
-        self.currentSongFrame = self.FRAMES.copy()
+        currentSongFrames = self.FRAMES.copy()
         keys  = self.FRAMES.keys()
         
         while self.fileCursor.tell() < self.id3Size < self.fileSize:
@@ -64,41 +80,35 @@ class Info(object):
                     for char in self.fileCursor.read(frameSize):
                         if char == 32 or 64 < char < 123 or 47 < char < 58:
                             info += chr(char)
-                    self.currentSongFrame[frameName] = info
+                    currentSongFrames[frameName] = info
                     
                 else:
-                    self.currentSongFrame[frameName] = f"{self.fileCursor.tell()}_{frameSize}"
+                    currentSongFrames[frameName] = f"{self.fileCursor.tell()}_{frameSize}"
                     self.fileCursor.seek(self.fileCursor.tell()+frameSize)
             else:
-                self.fileCursor.seek(self.fileCursor.tell()+frameSize)
+                self.fileCursor.seek(self.fileCursor.tell() + frameSize)
 
-        return self.currentSongFrame
-
-    def closeFile(self,):
-        """Closing the specified file and go for another."""
-
-        self.fileCursor.close()
-        self.fileCursor = None
-        
-    def exportImage(self,) -> None:
+        return currentSongFrames
+    
+    def exportImage(self, song_frames: dict) -> None:
         """exporting the image from current song based on APIC frame, if exists."""
-
+        
         byte: bytes = bytes()
         mimeType: str = str()
         seekSize: str = str()
         readSize: str = str()
-                
-        if self.currentSongFrame["APIC"]:
-            seekSize, readSize = self.currentSongFrame["APIC"].split('_')
 
+        if song_frames["APIC"] != "N/A":
+            seekSize, readSize = song_frames["APIC"].split('_')
+            
             if int(readSize) > 66:
                 self.fileCursor.seek(int(seekSize))
-
+                
                 while True:
                     byte = self.fileCursor.read(1)
-                
+                    
                     if byte == b"\xff":
-
+                        
                         if self.fileCursor.read(2) == b"\xd8\xff":
                             mimeType = "jpg"
                             self.fileCursor.seek(self.fileCursor.tell()-3)
@@ -106,9 +116,9 @@ class Info(object):
                         else:
                             self.fileCursor.seek(self.fileCursor.tell()-2)
                             continue
-
+                        
                     elif byte == b"\x89":
-
+                        
                         if self.fileCursor.read(7) == b"\x50\x4e\x47\x0d\x0a\x1a\x0a":
                             mimeType = "png"
                             self.fileCursor.seek(self.fileCursor.tell()-8)
@@ -117,27 +127,19 @@ class Info(object):
                             self.fileCursor.seek(self.fileCursor.tell()-7)
                             continue
 
-            with open(f"{self.fileCursor.name}.{mimeType}", "wb") as img:
-                img.write(self.fileCursor.read(int(readSize)))
-                img.close()
-
-                del img
-                
-    def exportXml(self,):
-        """Exporting the info as a xml."""
-        pass
-    
-if __name__ == "__main__":
-
-    musicList = listdir("/home/woozy/Music/m/")
-    musicList.sort()
-    for music in musicList:
-        ins = Info(f"/home/woozy/Music/m/{music}")
-        if ins.isValid:
-            ins.readHeader()
-            if ins.id3Version == b'\x03\x00' or b'\x04\x00':
-                print(music, ins.findFrames())
-                ins.exportImage()
-                ins.closeFile()
-                del ins
+                with open(f"APIC.{mimeType}", "wb") as img:
+                    img.write(self.fileCursor.read(int(readSize)))
+                    img.close()
                     
+                    del img
+                    
+                    song_frames['APIC'] = f"APIC.{mimeType}"
+                    
+                    return song_frames
+                
+    def closeFile(self,):
+        """Closing the specified file and go for another."""
+
+        self.fileCursor.close()
+        self.fileCursor = None
+                
